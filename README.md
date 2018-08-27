6bd866bf02d24e6de350ac59939d22d26ac7b200
Aga <rubin.samuel20@yahoo.ro>
2018-08-28 01:34:53 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,46 @@
+# steam-rep-bot
+## +REP BOT
+Auto comment you steam profile
+
+# Getting Started
+For you to get started you need the following
+```
+NODEJS
+NPM Package Installer
+Your Shared Secret
+```
+### Installing
+
+First off get your Identity Secret, and Shared Secret. Read this Thread here (https://forums.backpack.tf/index.php?/topic/46354-guide-how-to-find-the-steam-identity_secret-on-an-android-phone/)
+
+Or use SteamDesktopAuthenticator (https://github.com/Jessecar96/SteamDesktopAuthenticator.git)
+Enter the SDA folder in the maFiles folder open the file that has your SteamID name and look for Identity Secret and Shared Secret code.
+
+
+Fill in config.json
+
+After this open console, and write the following.
+```
+mkdir steam-rep-bot
+npm install steam-user
+npm install steam-tradeoffer-manager
+npm install steamcommunity
+npm install steam-totp
+npm install fs
+
+```
+After this make way to this directory, and drop in the code.
+Once this is complete type the following into Command Prompt.
+
+```
+cd steam-rep-bot
+node index.js
+```
+Or make a batch file where you put the following command
+```
+@echo off
+color 2
+node index.js
+pause
+```
+And save as start.bat in steam-rep-bot folder.
---
+++ index.js
@@ -0,0 +1,67 @@
+var SteamUser = require('steam-user');
+var SteamCommunity = require('steamcommunity');
+var SteamTotp = require('steam-totp');
+var TradeOfferManager = require('steam-tradeoffer-manager'); 
+var fs = require('fs');
+
+var firstClient = new SteamUser();
+
+// Managers
+var firstManager = new TradeOfferManager({
+   "steam": firstClient, 
+   "domain": "example.com", 
+   "language": "en" 
+});
+
+// Communities
+var firstCommunity = new SteamCommunity();
+
+// Config
+var config = JSON.parse(fs.readFileSync('./config.json'));
+
+var firstLogonOptions = {
+   "accountName": config.bots.firstaccount.username,
+   "password": config.bots.firstaccount.password,
+   "twoFactorCode": SteamTotp.getAuthCode(config.bots.firstaccount.shared_secret)
+};
+
+// Logging in...
+firstClient.logOn(firstLogonOptions);
+
+firstClient.on('loggedOn', () => {
+   console.log('Logged In!');
+   firstClient.setPersona(SteamUser.Steam.EPersonaState.Online);
+   firstClient.gamesPlayed(["Testing +REP BOT",440,570]); 
+});
+
+firstClient.on('webSession', (sessionid, cookies) => {
+firstManager.setCookies(cookies);
+firstCommunity.setCookies(cookies);
+});
+
+
+firstClient.on('friendRelationship', function(steamID, relationship) {
+ 
+     if (relationship == SteamUser.Steam.EFriendRelationship.RequestRecipient) {
+         firstClient.addFriend(steamID);
+         console.log(" ");
+         console.log("Accepted friend request from: " + steamID);
+         firstClient.chatMessage(steamID, "Welcome To My +REP Bot, type !help to get started!");
+     }
+     });
+
+
+
+firstClient.on("friendMessage", function(steamID, message) {
+if (message == "!help") {
+firstClient.chatMessage(steamID, "Type !comment for +rep on your profile");
+}
+});
+
+
+firstClient.on("friendMessage", function(steamID, message) {
+  if (message == "!comment") {
+    firstClient.chatMessage(steamID, "ok i comment you profile !")
+    firstCommunity.postUserComment(steamID, "+REP Signed by dife")
+  }
+});
